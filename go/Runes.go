b8ce22f2bdbc7713d06f2192ef85218e73a01747
mcdxwell <itsdavzz@gmail.com>
2021-12-13 22:38:25 -0600

    Rune literals

---
+++ go/Runes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Strings are used for lots of text characters
+	// GO's runes are used to represent a single text character
+	fmt.Println('A')
+}
+
+// rune literals use single quotation marks (')
+// string literals use double quotation marks (")
