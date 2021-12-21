84996d293ba47c4315bf992287c80794bc51364b
David McDowell <68706889+mcdxwell@users.noreply.github.com>
2021-12-20 22:16:32 -0500

    Create funky.go
    Practicing on mobile in Cancun/Playa

---
+++ go/Chapter4/funky.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+"fmt"
+"bufio"
+)
+
+// Fix, done at Playa on mobile
+func getName () {
+input := NewReader()
+name := input
+fmt.Println(name)
+}
+
+
+func main() {
+
+// Call getName
+
+getName()
+}
