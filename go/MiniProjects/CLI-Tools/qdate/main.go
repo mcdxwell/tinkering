cc6c6021cf1198ee9356749cdd5fd7bcd2585c99
mcdxwell <itsdavzz@gmail.com>
2022-01-01 22:16:04 -0600

    get days function fixed

--- go/MiniProjects/CLI-Tools/qdate/main.go
+++ go/MiniProjects/CLI-Tools/qdate/main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 )
 
 func main() {
-	current_date := time.Now().Local()
-	fmt.Println("The current date is: ", current_date.Format("01-02-2006"))
-
-	desired_date := current_date.Add(time.Hour * 24 * 10)
-	// %d
-	fmt.Println("The date in days: ", desired_date.Format("01-02-2006"))
+	getDays()
 }
 
 func getDays() {
 	// work in progress
-	/* days := flag.String("d", "d6", "Measures of time: day, month, year. Format: dX where X is the number of days")
+
+	current_date := time.Now().Local()
+	fmt.Println("The current date is: ", current_date.Format("01-02-2006"))
+	days := flag.String("d", "d20", "Measures of time: day, month, year. Format: dX where X is the number of days")
 
 	numDays := (*days)[1:]
 	d, err := strconv.Atoi(numDays)
 	if err != nil {
 		log.Fatal(err)
 	}
-	*/
+
+	desired_date := current_date.Add(time.Hour * 24 * time.Duration(d))
+	// %d
+	fmt.Println("The date in days: ", desired_date.Format("01-02-2006"))
 
 }
