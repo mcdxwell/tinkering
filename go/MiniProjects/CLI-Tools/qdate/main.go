3c7a421591aa3d081792ca746432b1fe6918e31f
mcdxwell <itsdavzz@gmail.com>
2022-01-02 00:20:26 -0600

    getDate functions are almost done

--- go/MiniProjects/CLI-Tools/qdate/main.go
+++ go/MiniProjects/CLI-Tools/qdate/main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	getDays()
-	//getConv()
+	getMonths()
+	getYears()
 }
 
 // return type: time.Time
@@ -20,32 +21,27 @@ func getCurrDate() time.Time {
 	return current_date
 }
 
+// Do the same with getMonth() and getYear()
 func getDays() {
-	// TO-DO:
-	// make lines 25 to 31 one function
-	// ----
-	// completed
 	days := flag.String("d", "d22", "Measures of time: day, month, year. Format: dX where X is the number of days")
-
 	dd := getConv(*&days)
-
-	// TO-DO:
-	// use AddDate() instead of Add(), cleaner and more consistent
-	// https://pkg.go.dev/time#example-Time.Add
-	// ----
-	// completed
 	desired_dd := getCurrDate().AddDate(0, 0, dd)
-	// %d
 	fmt.Printf("The date in %d days: %v", dd, desired_dd.Format("01-02-2006"))
-
 }
 
 func getMonths() {
+	months := flag.String("m", "m22", "Measures of time: day, month, year. Format: mX where X is the number of months")
+	mm := getConv(*&months)
+	desired_mm := getCurrDate().AddDate(0, mm, 0)
+	fmt.Printf("The date in %d months: %v", mm, desired_mm.Format("01-02-2006"))
 
 }
 
 func getYears() {
-
+	years := flag.String("y", "y22", "Measures of time: day, month, year. Format: yX where X is the number of years")
+	yyyy := getConv(*&years)
+	desired_yyyy := getCurrDate().AddDate(yyyy, 0, 0)
+	fmt.Printf("The date in %d years: %v", yyyy, desired_yyyy.Format("01-02-2006"))
 }
 
 func getConv(toonies *string) int {
