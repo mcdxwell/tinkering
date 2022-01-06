a8fff396d0218c4ecdc9246b1c9d4ac661dde58f
mcdxwell <itsdavzz@gmail.com>
2022-01-05 23:56:01 -0600

    mergesort algorithm

---
+++ go/Algorithms/mergesort/mergesort.go
@@ -0,0 +1,44 @@
+package main
+
+// practice
+// https://github.com/TheAlgorithms/Go/blob/master/sort/mergesort.go
+func merge(a []int, b []int) []int {
+
+	var r = make([]int, len(a)+len(b))
+	var i = 0
+	var j = 0
+
+	for i < len(a) && j < len(b) {
+		if a[i] <= b[j] {
+			r[i+j] = a[i]
+			i++
+		} else {
+			r[i+j] = b[j]
+			j++
+
+		}
+	}
+
+	for i < len(a) {
+		r[i+j] = a[i]
+		i++
+	}
+
+	for j < len(b) {
+		r[i+j] = b[j]
+		j++
+	}
+	return r
+}
+
+func mergesort(items []int) []int {
+	if len(items) < 2 {
+		return items
+	}
+
+	var middle = len(items) / 2
+	var a = mergesort(items[:middle])
+	var b = mergesort(items[middle:])
+	return merge(a, b)
+
+}
