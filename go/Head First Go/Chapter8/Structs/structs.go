d169191a5286e7ac4e98bb428543e9b7d74db4c6
mcdxwell <itsdavzz@gmail.com>
2022-01-08 21:26:46 -0600

    CH8 exercise

--- go/Head First Go/Chapter8/Structs/structs.go
+++ go/Head First Go/Chapter8/Structs/structs.go
@@ -10,4 +10,18 @@ func main() {
 		enabled bool
 	}
 	fmt.Printf("%#v\n", myStruct)
+
+	var subscriber struct {
+		name   string
+		rate   float64
+		active bool
+	}
+
+	subscriber.name = "David"
+	subscriber.rate = 2.81
+	subscriber.active = true
+
+	fmt.Println("Name: ", subscriber.name)
+	fmt.Println("Monthly rate: ", subscriber.rate)
+	fmt.Println("Active?: ", subscriber.active)
 }
