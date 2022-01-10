d6f7972a2c1bd5e3f77f2f3ebd61f823d7381f21
mcdxwell <itsdavzz@gmail.com>
2022-01-09 18:11:47 -0600

    HFG structs

--- go/Head First Go/Chapter8/ModStruct/modstruct.go
+++ go/Head First Go/Chapter8/ModStruct/modstruct.go
@@ -1,2 +1,21 @@
 package main
 
+import "fmt"
+
+// we can use pointers to allow a function to update a struct
+
+type sub struct {
+	name   string
+	rate   float64
+	active bool
+}
+
+func applyDiscount(s *sub) {
+	s.rate = 4.99
+}
+
+func main() {
+	var s sub
+	applyDiscount(&s)
+	fmt.Println(s.rate)
+}
