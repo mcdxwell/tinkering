101bdf76889534c48a23f96372adad79ef30ce53
mcdxwell <itsdavzz@gmail.com>
2022-01-15 17:19:58 -0600

    updated

---
+++ go/ExMachina/exmacode.go
@@ -0,0 +1,26 @@
+package main
+
+func sieve() {
+	x := [10000]int{}
+
+	return x
+}
+
+func prime(n int, x []int) int {
+	i := 1
+	j := 1
+
+	for j <= n {
+		if x[i] == 1 {
+			j = j + 1
+		}
+		i = i + 1
+	}
+	return i - 1
+}
+func main() {
+	x := sieve()
+	code := [4]int{1206, 301, 384, 5}
+	key := [4]int{1, 1, 2, 2}
+
+}
--- go/Head First Go/README.md
+++
@@ -1,7 +0,0 @@
-# Head First Go - Notes
-
-Chapter by chapter, I followed along and implemented some of the provided exercises.
-The code/explanations are similar, modified, and sometimes directly taken from the book.
-Do NOT use this for commercial purposes
-
-(Personal Note Taking)
