a3b29d63b05beb85064b2158260484a6122b07db
mcdxwell <itsdavzz@gmail.com>
2022-02-01 23:56:09 -0600

    Project Euler P4

--- go/ProjectEuler/Problem4/p4.go
+++ go/ProjectEuler/Problem4/p4.go
@@ -1,34 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
-	"strings"
 )
 
 // TO-DO:
 
 // Refer to this: https://projecteuler.net/thread=4;page=5
-
-func isPalindrome(x int) bool {
-
-	s := strconv.Itoa(x)
-	fmt.Println("First s:", s)
-	fmt.Println("Len(s): ", len(s))
-
-	// append each character of s to slice
-	// slice is currently empty.
-	slice := make([]string, len(s))
-
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		slice[i], slice[j] = slice[j], slice[i]
+// reference: https://go.dev/play/p/5FUOzjBa-o
+func isPalindrome(input int) bool {
+	x := strconv.Itoa(input)
+	for i := 0; i < len(x)/2; i++ {
+		if x[i] != x[len(x)-i-1] {
+			return false
+		}
 	}
 
-	fmt.Println(slice)
-	ss := strings.Join(slice, ",")
-	fmt.Println(ss)
-	fmt.Println("S:", s)
-	return s == ss
+	return true
 }
 
 func main() {
