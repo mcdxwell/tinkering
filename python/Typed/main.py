134462db1e9e5722ea3ab8e6e6de6bd8488cf929
mcdxwell <itsdavzz@gmail.com>
2022-02-19 16:01:03 -0600

    Match statements in python (example)

--- python/Typed/main.py
+++ python/Typed/main.py
@@ -22,3 +22,74 @@ def talk(func, txt: str) -> str:
 
 speak = talk(yell, "hello")
 print(speak)
+
+
+
+foods = {'bread':2.00, 'eggs': 1.24, 'oranges': 5.47}
+
+for food, price in foods.copy().items():
+    if price > 5.0:
+        del foods[food]
+
+cheaper_food = {}
+for food, price in foods.items():
+    if price < 5.0:
+        cheaper_food[food] = price
+
+print(cheaper_food)
+
+# interesting tidbit of code
+for i in range(len(list(range(5)))):
+    print(i)
+
+# Match statements (example)
+def tx_area_codes(code) -> str:
+    match code:
+        case 210:
+            return "San Antonio"
+        case 214 | 469 | 972:
+            return "Dallas/Plano"
+        case 254:
+            return "Waco/Killeen" 
+        case 281 | 346 | 713 | 832:
+            return "Houston"    
+        case 325:
+            return "Abilene"
+        case 361:
+            return "Corpus Christi/Victoria"
+        case 409:
+            return "Beaumont/Galveston"
+        case 430 | 903:
+            return "Texarkana, Tyler, Paris"
+        case 432:
+            return "Midland/Odessa"
+        case 512 | 737:
+            return "Austin"
+        case 682 | 817:
+            return "Arlington/Fort Worth"
+        case 806:
+            return "Amarillo/Lubbock"
+        case 830: 
+            return "Seguin/Uvalde"
+        case 915:
+            return "El Paso"
+        case 936:
+            return "Huntsville/Nacogdoches"
+        case 940:
+            return "Denton/Wichita Falls"
+        case 956:
+            return "Brownsville/Laredo"
+        case 979:
+            return "Bryan/College Station"
+
+print(tx_area_codes(281))
+# Output: Houston
+
+
+
+
+
+
+
+
+
