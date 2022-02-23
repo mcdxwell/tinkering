544bc30913014e4a5f07dbb6a34f733bf9520775
mcdxwell <itsdavzz@gmail.com>
2022-02-22 21:28:18 -0600

    Changed function name to camelCase

--- go/ProjectEuler/Problem9/p9.go
+++ go/ProjectEuler/Problem9/p9.go
@@ -2,18 +2,15 @@ package main
 
 import "fmt"
 
-// https://projecteuler.net/problem=9
-
-func test_triplet(a int, b int, c int) bool {
+func testTriplet(a int, b int, c int) bool {
 	return (a*a + b*b) == c*c
 }
 
-func get_triplets(limit int) {
+func getTriplets(limit int) {
 	a, b, c := 0, 0, 0
 	m := 2
 
 	for c < limit {
-
 		for n := 1; n <= m-1; n++ {
 
 			a = m*m - n*n
@@ -24,7 +21,7 @@ func get_triplets(limit int) {
 				break
 			}
 
-			if test_triplet(a, b, c) {
+			if testTriplet(a, b, c) {
 				if a+b+c == 1000 {
 					prod := a * b * c
 					fmt.Printf("The product of %d %d %d = %d", a, b, c, prod)
@@ -37,5 +34,5 @@ func get_triplets(limit int) {
 }
 
 func main() {
-	get_triplets(1000)
+	getTriplets(1000)
 }
