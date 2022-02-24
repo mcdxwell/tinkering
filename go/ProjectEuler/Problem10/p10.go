49531956bb29b4d8eb2408ee3cc1b2223804ffe6
mcdxwell <itsdavzz@gmail.com>
2022-02-24 17:34:17 -0600

    PE P10 Solved - Sieve of Atkins

--- go/ProjectEuler/Problem10/p10.go
+++ go/ProjectEuler/Problem10/p10.go
@@ -2,47 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func sieveOfAtkins(ch chan int, limit int) {
-	if limit > 2 {
-		fmt.Printf("2 ")
-	}
 
-	if limit > 3 {
-		fmt.Printf("3 ")
-	}
-
-	sieve := make([]bool, limit+1)
-
+	sieve := make([]bool, limit)
+	limitSqrt := int(math.Sqrt(float64(limit)))
 	for i := 0; i < limit; i++ {
-		sieve = append(sieve, false)
+		sieve[i] = false
 	}
 
-	for x := 1; x*x < limit; x++ {
-		for y := 1; y*y < limit; y++ {
-
-			n := 4*x*x + y*y
-			if n <= limit && n%12 == 1 || n%12 == 5 {
-
-				// uncertain
-				if sieve[n] != true {
-					sieve[n] = true
-				}
+	var n int
+	for x := 1; x <= limitSqrt; x++ {
+		for y := 1; y <= limitSqrt; y++ {
+			n = (4 * x * x) + (y * y)
+			if n <= limit && (n%12 == 1 || n%12 == 5) {
+				sieve[n] = !sieve[n]
 			}
 
-			n = 3*x*x + y*y
+			n = (3 * x * x) + (y * y)
 			if n <= limit && n%12 == 7 {
-				if sieve[n] != true {
-					sieve[n] = true
-				}
+				sieve[n] = !sieve[n]
 			}
 
-			n = 3*x*x - y*y
+			n = (3 * x * x) - (y * y)
 			if x > y && n <= limit && n%12 == 11 {
-				if sieve[n] != true {
-					sieve[n] = true
-				}
+				sieve[n] = !sieve[n]
 			}
 		}
 	}
@@ -55,12 +41,14 @@ func sieveOfAtkins(ch chan int, limit int) {
 		}
 	}
 
+	ch <- 2
+	ch <- 3
 	for a := 5; a < limit; a++ {
 		if sieve[a] {
 			ch <- a
 		}
 	}
-
+	close(ch)
 }
 
 func main() {
@@ -70,4 +58,5 @@ func main() {
 	for prime := range ch {
 		prime_sum += uint64(prime)
 	}
+	fmt.Println(prime_sum)
 }
