cb85b078db1d7c71611a604562e5bc1aaf78b17c
mcdxwell <itsdavzz@gmail.com>
2022-03-15 20:35:40 -0500

    First generics

---
+++ go/generics/main.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	int64 | float64
+}
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// Generic Function
+
+// SumIntsOrFloats sums the values of map
+func SumIntsOrFloats[k comparable, V Number](m map[k]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+type Num interface {
+	int
+}
+
+func AddOddGrades[k comparable, V Num](m map[k]V) []V {
+	var evenSum V
+	var oddSum V
+	for _, v := range m {
+		if v%2 != 0 {
+			oddSum += v
+		} else {
+
+			evenSum += v
+		}
+	}
+	var ff []V
+	ff = append(ff, oddSum)
+	ff = append(ff, evenSum)
+
+	return ff
+}
+
+func main() {
+
+	ints := map[string]int64{
+		"first":  10,
+		"second": 12,
+		"third":  13,
+	}
+
+	floats := map[string]float64{
+		"first":  10.97,
+		"second": 12.95,
+		"third":  13.94,
+	}
+
+	moreInts := map[string]int{
+		"first":   10,
+		"second":  12,
+		"third":   13,
+		"fourth":  13,
+		"fifth":   13,
+		"sixth":   13,
+		"seventh": 13,
+		"eighth":  13,
+	}
+
+	fmt.Printf("Non-generic sums: %v and %v\nGeneric sums: %v and %v\n", SumInts(ints), SumFloats(floats), SumIntsOrFloats(ints), SumIntsOrFloats(floats))
+	fmt.Printf("Generic odd sums: %v Even sums: \n", AddOddGrades(moreInts))
+}
