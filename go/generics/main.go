34027ab55e09e9ea3046f389fb8f9c7643578e03
mcdxwell <itsdavzz@gmail.com>
2022-03-17 18:14:53 -0500

    Working more with generics

--- go/generics/main.go
+++ go/generics/main.go
@@ -37,7 +37,7 @@ func SumIntsOrFloats[k comparable, V Number](m map[k]V) V {
 }
 
 type Num interface {
-	int
+	int64
 }
 
 func AddOddGrades[k comparable, V Num](m map[k]V) []V {
@@ -78,7 +78,7 @@ func main() {
 		"third":  13.94,
 	}
 
-	moreInts := map[string]int{
+	moreInts := map[string]int64{
 		"first":   10,
 		"second":  12,
 		"third":   13,
@@ -94,6 +94,17 @@ func main() {
 
 	roonies := []rune{97, 20, 12, 12, 12, 13, 97, 10, 10, 1, 2, 3, 4, 4, 5}
 	fmt.Println(Set(roonies))
+
+	fmt.Println(stuff(10, 12, min))
+	fmt.Println(stuff(10, 12, max))
+	fmt.Println(stuff(10, 10, max))
+	hhh := []int{1, 2, 3}
+	jjj := []int{1, 2, 3, 4}
+	oo, ii := twoLen(hhh, jjj)
+	fmt.Println(stuff(oo, ii, max))
+
+	fmt.Println(manyLen(jjj, hhh))
+	fmt.Println(manyLen(moreInts, ints))
 }
 
 // Removes duplicates
@@ -107,4 +118,56 @@ func Set[T any](s []T) []T {
 		}
 	}
 	return list
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+type rawr func(int, int) int
+
+// func take two ints and rawr func
+func stuff(a, b int, f rawr) int {
+	return f(a, b)
+}
+
+type inters interface {
+	[]uint | []int | []int8 | []int16 | []int64
+}
+
+type floaters interface {
+	[]float32 | []float64
+}
+
+type mappers interface {
+	map[int]int | map[string]string | map[string]int64
+}
+
+type thingers interface {
+	[]rune | inters | floaters | mappers
+}
+
+// Return two lengths
+func twoLen[T thingers](a T, b T) (int, int) {
+	return len(a), len(b)
+}
+
+// Return a slice of lengths
+func manyLen[T thingers](arrs ...T) []int {
+	lengths := make([]int, 0)
+	for _, arr := range arrs {
+		arrLen := len(arr)
+		lengths = append(lengths, arrLen)
+	}
+	return lengths
 }
